eac0da2deb505d8577f1a853a1ed5f07aa351ce6
usama <mahmoodify@gmail.com>
2021-07-11 21:23:58 +0500

    Added the blank files to folders and vscode directory in .gitignore

---
+++ src/Core/Orchestrator/OrchestratorMain.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "./PlatformTest.h"
+#include "./ConfigurationProvider/ConfigurationProvider.h"
+#include "./ConnectionManager/TcpConnectionManager.h"
+#include "./Crypto/CryptoServiceProvider.h"
+#include "./ModulesManager/ModulesExecutionManager.h"
+
+int main(int argc, char** argv)
+{
+    printf("Starting Orchestra");
+}
